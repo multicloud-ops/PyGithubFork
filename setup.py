ec0f1bd40860e44b58062840375267d1db88fc0c
eveliososa <44717805+eveliososa@users.noreply.github.com>
2021-05-24 15:02:43 -0500

    Update requirements (#3)
    * Update setup.py
    
    * Update python-publish.yml
    
    * Update setup.py
    
    * Update requirements.txt

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='PyGithubFork',
-    version='default',
+    version='0.0.0',
     description='Code wraps PyGithub to simplify workflows dealing with forks',
     author='hassenius',
     url='https://github.com/multicloud-ops/PyGithubFork',
     packages=['githubfork'],
     install_requires=[
-        'pygithub'
+        'pygithub==1.54.1'
     ]
 )
